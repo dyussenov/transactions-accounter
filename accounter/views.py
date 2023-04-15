a8031ed24dd9d53104950504c414ec54ff657d7a
dyussenov <dyusenov.aset.arm@gmail.com>
2023-04-16 04:46:11 +0600

    add suppliers

--- accounter/views.py
+++ accounter/views.py
@@ -1,12 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .forms import *
-
+from .models import *
 
 def home(request):
     context = {
-        'customer_form': CustomerForm,
         'supplier_form': SupplierForm,
         'operation_from': OperationForm,
     }
     return render(request, 'accounter/home.html', context)
+
+
+def customers(request):
+    if request.method == 'POST':
+        form = CustomerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('customers')
+
+    context = {
+        'customer_form': CustomerForm,
+        'customers': Customer.objects.all()
+    }
+    return render(request, 'accounter/customers.html', context)
+
+def suppliers(request):
+    if request.method == 'POST':
+        form = SupplierForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('suppliers')
+
+    context = {
+        'supplier_form': SupplierForm,
+        'suppliers': Supplier.objects.all()
+    }
+    return render(request, 'accounter/suppliers.html', context)
